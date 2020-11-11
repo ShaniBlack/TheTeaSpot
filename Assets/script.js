766d38b47b53f14cc4fe857e5cc9307e0baf10bc
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-11 12:27:21 -0600

    added multiple html for different pages

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
 // Search Bar 
-$("#inpt_search").on('focus', function () {
-	$(this).parent('label').addClass('active');
-});
+// $("#inpt_search").on('focus', function () {
+// 	$(this).parent('label').addClass('active');
+// });
 
-$("#inpt_search").on('blur', function () {
-	if($(this).val().length == 0)
-		$(this).parent('label').removeClass('active');
-});
+// $("#inpt_search").on('blur', function () {
+// 	if($(this).val().length == 0)
+// 		$(this).parent('label').removeClass('active');
+// });
