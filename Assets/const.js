26cbe9e165eca22603209272c031386f440a4920
Shani Black <shaniblack2004@gmail.com>
2020-11-13 10:32:53 -0600

    created event listener on click of title to be stored in local storage/also created object for NYArray so names of NYArray matched oneNews Array

--- Assets/const.js
+++ Assets/const.js
@@ -43,26 +43,55 @@ $(".btn").on("click", setValue);
 
 //////////////
 // recentlyViewed 
-let recentTitle = document.querySelector(".titleNews");
-console.log(recentTitle);
+let container = document.querySelector(".container");
+let recentImage = document.querySelector(".newsImage");
 let recentDescription = document.querySelector(".description");
-console.log(recentDescription);
-let recentImage = document.querySelector("col-6");
-console.log(recentImage);
+
+let recentLink = [];
+let storedNews = JSON.parse(localStorage.getItem("recentLink"));
+if (storedNews != null) {
+  recentLink = storedNews;
+}
 
 
-let recentlyViewedArray = [];
+// getRecents();
 
-let clickedTitles;
+// function addRecents();
+// for (let i = 0; i < 9; i++) {
+//   const addRecent = addRecents[i];
+  
+// }
 
-$(".title").on("click", function(event){
-    event.preventDefault();
-    recentlyViewedArray.prepend(view);
-    console.log(recentlyViewedArray);
-    
-    getViewed(clickedTitles)
+
+
+// 
+  
+// }
+
+function clickedNews() {
+  localStorage.setItem("recentLink", JSON.stringify(recentLink));
+
+}
+
+container.addEventListener("click", function(event) {
+ if (event.target.matches("h3")) {
+  console.log(event.target)
+  event.preventDefault();
+
+ let clickedI = (event.target.dataset.i);
+ let clickedArray = (event.target.dataset.array);
+ let storedHREF = "";
+
+ if (clickedArray === "NYArray") {
+ recentLink.push(NYArray[clickedI])
+  storedHREF = NYArray[clickedI].url;
+ }else{
+ recentLink.push(oneNewsArray[clickedI])
+ storedHREF = oneNewsArray[clickedI].url;
+
+}
+clickedNews()
+ 
+window.location = storedHREF
+}
 })
-     let view = function() {
-        localStorage.setItem("views", JSON.stringify(views));
-        console.log(view);
-     }
